55f7e6b4b01b3580aa9270106b0027a9866b8a3e
Sungguk Han <sungguk80@gmail.com>
2017-02-24 18:07:07 +0900

    Add files via upload

--- applications/sentiment/sentiment_model.py
+++ applications/sentiment/sentiment_model.py
@@ -7,7 +7,9 @@
 import tensorflow as tf
 from hparams import HParams
 
-from tensorflow.python.ops import rnn, rnn_cell
+#from tensorflow.python.ops import rnn, rnn_cell
+import tensorflow.contrib.rnn as rnn
+from tensorflow.python.ops import rnn as bi_rnn
 from tensorflow.contrib.layers.python.layers import linear
 
 class Sentiment(object):
@@ -27,13 +29,14 @@ class Sentiment(object):
         def _embedding(x):
             # character embedding 
             shape       = [hps.vocab_size, hps.emb_size]
-            initializer = tf.uniform_unit_scaling_initializer(dtype=tf.float32, full_shape=shape)
+            initializer = tf.uniform_unit_scaling_initializer(dtype=tf.float32)
             emb_mat     = tf.get_variable("emb", shape, initializer=initializer, dtype=tf.float32)
 
             input_emb   = tf.nn.embedding_lookup(emb_mat, x)   # [batch_size, sent_len, emb_dim]
 
             # split input_emb -> num_steps
-            step_inputs = tf.unpack(input_emb, axis=1)
+            #step_inputs = tf.unpack(input_emb, axis=1)
+            step_inputs = tf.unstack(input_emb, axis=1)
             return step_inputs
 
         def _sequence_dropout(step_inputs, keep_prob):
@@ -46,10 +49,10 @@ class Sentiment(object):
             return step_outputs
 
         def _sentence_encoding(step_inputs, seq_length, cell_size):
-            f_rnn_cell = tf.nn.rnn_cell.LSTMCell(cell_size, state_is_tuple=True)
-            b_rnn_cell = tf.nn.rnn_cell.LSTMCell(cell_size, state_is_tuple=True)
-            _inputs = tf.pack(step_inputs, axis=1)
-            outputs, states, = rnn.bidirectional_dynamic_rnn(f_rnn_cell,
+            f_rnn_cell = rnn.LSTMCell(cell_size, state_is_tuple=True)
+            b_rnn_cell = rnn.LSTMCell(cell_size, state_is_tuple=True)
+            _inputs = tf.stack(step_inputs, axis=1)
+            outputs, states, = bi_rnn.bidirectional_dynamic_rnn(f_rnn_cell,
                                                              b_rnn_cell,
                                                              _inputs,
                                                              sequence_length=tf.cast(seq_length, tf.int64),
@@ -62,10 +65,11 @@ class Sentiment(object):
             output_fw, output_bw = outputs
             states_fw, states_bw = states
 
-            steps_fw = tf.unpack(output_fw, axis=1)
-            steps_bw = tf.unpack(output_bw, axis=1)
+            steps_fw = tf.unstack(output_fw, axis=1)
+            steps_bw = tf.unstack(output_bw, axis=1)
 
-            sent_encoding = tf.concat(1, [steps_fw[0], steps_bw[0]]) # [batch_size]
+            #sent_encoding = tf.concat(1, [steps_fw[0], steps_bw[0]]) # [batch_size]
+            sent_encoding = tf.concat([steps_fw[0], steps_bw[0]],1) # [batch_size]
             return sent_encoding
 
         def _to_class(input, num_class):
@@ -76,7 +80,7 @@ class Sentiment(object):
             # out : [batch_size, num_class] float - unscaled logits
             # ref : [batch_size] integer
             # calculate loss function using cross-entropy
-            batch_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(out, ref, name="sentiment_loss") # [batch_size]
+            batch_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=out, labels=ref, name="sentiment_loss") # [batch_size]
             loss = tf.reduce_mean(batch_loss)
             return loss
         
@@ -95,14 +99,17 @@ class Sentiment(object):
         self.out_probs = out_probs
         self.out_pred  = out_pred
 
-        tf.scalar_summary("model/loss", self.loss)
-        self.global_step = tf.get_variable("global_step", [], tf.int32, initializer=tf.zeros_initializer, trainable=False)
+        #tf.scalar_summary("model/loss", self.loss)
+        tf.summary.scalar("model/loss", self.loss)
+        #self.global_step = tf.get_variable("global_step", [], tf.int32, initializer=tf.zeros_initializer, trainable=False)
+        self.global_step = tf.get_variable("global_step", [], tf.int32, initializer=tf.zeros_initializer(), trainable=False)
 
         if mode == "train":
             #optimizer       = tf.train.AdagradOptimizer(hps.learning_rate, initial_accumulator_value=1.0)
             optimizer       = tf.train.AdamOptimizer(hps.learning_rate)
             self.train_op   = optimizer.minimize(self.loss, global_step=self.global_step)
-            self.summary_op = tf.merge_all_summaries()
+            #self.summary_op = tf.merge_all_summaries()
+            self.summary_op = tf.summary.merge_all
         else:
             self.train_op = tf.no_op()
 
